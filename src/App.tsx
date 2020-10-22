aedaa2c3d080317b152bfa6cce6bd2c7770237bc
kmidtlid3@gmail.com <kmidtlid3@gmail.com>
2020-10-22 09:37:50 +0200

    Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { Alert } from 'reactstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <Alert color="success">Everything is working!</Alert>
+    </div>
+  );
+}
+
+export default App;
